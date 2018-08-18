78e35baa8a20eeda77dc23b90efc81e2429051fc
everdrone <giorgiotropiano@gmail.com>
2018-08-18 07:27:47 +0200

    Add obfus formula

---
+++ obfus.rb
@@ -0,0 +1,23 @@
+class Obfus < Formula
+  desc "File compression and encryption tool"
+  homepage ""
+  url "https://github.com/everdrone/obfus/archive/v0.1.1.tar.gz"
+  sha256 "33dfe9736edcb3229e07af7aaf8328445324c28702dcf07de98aef5c1c5bd301"
+
+  def install
+    bin.install "obfus"
+  end
+
+  # test do
+  #   # `test do` will create, run in and delete a temporary directory.
+  #   #
+  #   # This test will fail and we won't accept that! For Homebrew/homebrew-core
+  #   # this will need to be a test that verifies the functionality of the
+  #   # software. Run the test with `brew test obfus`. Options passed
+  #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+  #   #
+  #   # The installed folder is not in the path, so use the entire path to any
+  #   # executables being tested: `system "#{bin}/program", "do", "something"`.
+  #   system "false"
+  # end
+end
