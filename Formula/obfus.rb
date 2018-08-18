ee807754b12af74105a028b5517f0f20d5f66c46
everdrone <giorgiotropiano@gmail.com>
2018-08-18 18:20:12 +0200

    Update to v0.1.3

--- Formula/obfus.rb
+++ Formula/obfus.rb
@@ -1,8 +1,10 @@
 class Obfus < Formula
   desc "File compression and encryption tool"
   homepage "https://github.com/everdrone/obfus"
-  url "https://github.com/everdrone/obfus/archive/v0.1.2.tar.gz"
-  sha256 "6d12d80a619c50945f87a439f24263b1999cbd419af5baa2577608d8dc9c2857"
+  url "https://github.com/everdrone/obfus/archive/v0.1.3.tar.gz"
+  sha256 "91a54166bd1b9d7350f489e8894720e4f91eff9a4430f144593195d68dcd91ae"
+
+  epends_on "ruby" if MacOS.version <= :sierra
 
   def install
     system "rake", "install", "prefix=#{prefix}"
