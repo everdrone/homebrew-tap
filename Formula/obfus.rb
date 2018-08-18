21520ff2777a624b79a6226f3b8a5954283df92b
everdrone <giorgiotropiano@gmail.com>
2018-08-18 18:54:14 +0200

    Add Formula dependencies (gnupg, brotli)

--- Formula/obfus.rb
+++ Formula/obfus.rb
@@ -5,6 +5,8 @@ class Obfus < Formula
   sha256 "4f954d2308c1433007817a79e73d83dd90ee343a615282a36fcdc8f5412e6bce"
 
   epends_on "ruby" if MacOS.version <= :sierra
+  depends_on "brotli"
+  depends_on "gnupg"
 
   def install
     system "rake", "mklink", "prefix=#{prefix}"
