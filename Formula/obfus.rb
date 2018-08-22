f5af0fa4c0e9e6a587bc67a1026684c7c6c057d8
everdrone <giorgiotropiano@gmail.com>
2018-08-22 05:00:27 +0200

    Add .git repo with revision and tag, update install function

--- Formula/obfus.rb
+++ Formula/obfus.rb
@@ -1,15 +1,24 @@
 class Obfus < Formula
   desc "File compression and encryption tool"
   homepage "https://github.com/everdrone/obfus"
-  url "https://github.com/everdrone/obfus/archive/v0.1.5.tar.gz"
-  sha256 "4f954d2308c1433007817a79e73d83dd90ee343a615282a36fcdc8f5412e6bce"
+  url "https://github.com/everdrone/obfus.git",
+    :tag => "v0.1.5"
+    :revision => "014ed7cdd1f547ce420ea90f7463636c42cb8922"
 
   epends_on "ruby" if MacOS.version <= :sierra
-  depends_on "brotli"
-  depends_on "gnupg"
+  # depends_on "brotli"
+  # depends_on "gnupg"
+
+  # def install
+  #   system "rake", "mklink", "prefix=#{prefix}"
+  # end
 
   def install
-    system "rake", "mklink", "prefix=#{prefix}"
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "obfus.gemspec"
+    system "gem", "install", "obfus-#{version}.gem"
+    bin.install libexec/"bin/obfus"
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
   end
 
   # test do
