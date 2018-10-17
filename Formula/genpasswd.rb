4b3e0bbbb622b7b903f93353f24c69ea01cad6e1
everdrone <giorgiotropiano@gmail.com>
2018-10-17 13:51:00 +0200

    Change install function for genpasswd

--- Formula/genpasswd.rb
+++ Formula/genpasswd.rb
@@ -1,14 +1,12 @@
 class Genpasswd < Formula
   desc "Generates iOS style strong password suggested by Safari"
   homepage ""
-  url "https://github.com/everdrone/genpasswd/archive/v0.1.1.tar.gz"
-  sha256 "f630c6eebcf936e7c5c7c84cd45fa9537125e87a342e1cce10a1d4bab8d1965c"
+  url "https://github.com/everdrone/genpasswd/archive/v0.1.2.tar.gz"
+  sha256 "b74cb0b957df9a88eac76dba9908f01156a8125edb7f73d19ffde5465cedae24"
+
+  depends_on "ruby" if MacOS.version <= :sierra
 
   def install
-    ENV['GEM_HOME'] = libexec
-    system 'gem', 'build', 'genpasswd.gemspec'
-    system 'gem', 'install', "genpasswd-#{version}.gem"
-    bin.install libexec/"bin/genpasswd"
-    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV['GEM_HOME'])
+    system "rake", "mklink", "prefix=#{prefix}"
   end
 end
