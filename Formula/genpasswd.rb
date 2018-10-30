14e9a87062001db680ea2d575ae5cd7d4ae0c3f3
everdrone <giorgiotropiano@gmail.com>
2018-10-30 11:27:44 +0100

    Update genpasswd to v0.1.3

--- Formula/genpasswd.rb
+++ Formula/genpasswd.rb
@@ -1,8 +1,8 @@
 class Genpasswd < Formula
   desc 'Generates iOS style strong password suggested by Safari'
   homepage ''
-  url 'https://github.com/everdrone/genpasswd/archive/v0.1.2.tar.gz'
-  sha256 'b74cb0b957df9a88eac76dba9908f01156a8125edb7f73d19ffde5465cedae24'
+  url 'https://github.com/everdrone/genpasswd/archive/v0.1.3.tar.gz'
+  sha256 '17abe514f2ebe1b82e13153cec3a1913144a94d68d84a607f1e6fa79350ba6cb'
 
   depends_on :ruby if MacOS.version <= :sierra
 
