97a6153dfc8cd7f0c63823e87191a0a24efe29d3
everdrone <giorgiotropiano@gmail.com>
2018-10-30 11:55:33 +0100

    Revert to v0.1.2 install script

--- Formula/genpasswd.rb
+++ Formula/genpasswd.rb
@@ -4,15 +4,9 @@ class Genpasswd < Formula
   url 'https://github.com/everdrone/genpasswd/archive/v0.1.3.tar.gz'
   sha256 '17abe514f2ebe1b82e13153cec3a1913144a94d68d84a607f1e6fa79350ba6cb'
 
-  depends_on 'ruby' if MacOS.version <= :sierra
+  depends_on "ruby" if MacOS.version <= :sierra
 
   def install
-    ENV["GEM_HOME"] = libexec
-
-    system "gem", "build", "genpasswd.gemspec"
-    system "gem", "install", "--ignore-dependencies", "genpasswd-#{version}.gem"
-
-    bin.install libexec/"bin/genpasswd"
-    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
+    system "rake", "mklink", "prefix=#{prefix}"
   end
 end
