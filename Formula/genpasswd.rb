4a3fd97771053d435942b564f0cfb3afc820be1a
everdrone <giorgiotropiano@gmail.com>
2018-10-30 11:39:37 +0100

    Add libexec prefix

--- Formula/genpasswd.rb
+++ Formula/genpasswd.rb
@@ -4,7 +4,7 @@ class Genpasswd < Formula
   url 'https://github.com/everdrone/genpasswd/archive/v0.1.3.tar.gz'
   sha256 '17abe514f2ebe1b82e13153cec3a1913144a94d68d84a607f1e6fa79350ba6cb'
 
-  depends_on :ruby if MacOS.version <= :sierra
+  depends_on 'ruby' if MacOS.version <= :sierra
 
   def install
     ENV["GEM_HOME"] = libexec
@@ -12,6 +12,7 @@ class Genpasswd < Formula
     system "gem", "build", "genpasswd.gemspec"
     system "gem", "install", "--ignore-dependencies", "genpasswd-#{version}.gem"
 
-    bin.install "bin/genpasswd"
+    bin.install libexec/"bin/genpasswd"
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
   end
 end
