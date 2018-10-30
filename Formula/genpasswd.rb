f284b30a9bfe27b150eae18fddcf51cf3f41c6da
everdrone <giorgiotropiano@gmail.com>
2018-10-30 11:19:46 +0100

    Use gem command inside install script

--- Formula/genpasswd.rb
+++ Formula/genpasswd.rb
@@ -4,11 +4,11 @@ class Genpasswd < Formula
   url 'https://github.com/everdrone/genpasswd/archive/v0.1.2.tar.gz'
   sha256 'b74cb0b957df9a88eac76dba9908f01156a8125edb7f73d19ffde5465cedae24'
 
-  depends_on 'ruby' if MacOS.version <= :sierra
-  depends_on 'genpasswd' => :ruby
+  depends_on :ruby if MacOS.version <= :sierra
 
   def install
-    # system "rake", "mklink", "prefix=#{prefix}"
-    bin.install 'bin/genpasswd'
+    system "gem", "build", "genpasswd.gemspec"
+    system "gem", "install", "--ignore-dependencies", "genpasswd-#{version}.gem"
+    bin.install "bin/genpasswd"
   end
 end
